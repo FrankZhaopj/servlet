706bc0264d9f6deeb415181dd739aa79b14b0355
FrankZhaopj <399109168@qq.com>
2017-09-22 10:01:33 +0800

    s

--- servlet/src/main/java/com/zhaopj/servlet/MyServlet_1.java
+++ servlet/src/main/java/com/zhaopj/servlet/MyServlet_1.java
@@ -2,7 +2,6 @@ package com.zhaopj.servlet;
 
 import java.io.IOException;
 
-import javax.jws.WebService;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
